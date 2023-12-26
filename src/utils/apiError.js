5ce06aaa0a17428a3dae8b3dd3e5da3df51e09a6
divyanshugarg2017 <divyanshugarg1995@gmail.com>
2023-12-26 12:49:11 -0800

    added utile and errorhandlers

---
+++ src/utils/apiError.js
@@ -0,0 +1,26 @@
+
+//contains class for error handling provided by node js by default
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message="Something went wrong",
+        errors=[],
+        stack=""
+    ){
+        super(message)
+        this.statusCode=statusCode
+        this.data=null          //read more about it
+        this.message=message
+        this.success=false
+        this.errors=errors
+
+        if(stack){
+            this.stack=stack
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+
+    }
+}
+
+export {ApiError}

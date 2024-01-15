c36acb232f300fea6595b4bd1e4f712f74538257
divyanshugarg2017 <divyanshugarg1995@gmail.com>
2024-01-15 12:14:24 -0800

    added user login controller logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -71,4 +71,57 @@ const registerUser = asyncHandler( async(req,res)=>{
    //return response
 } )
 
-export {registerUser} 
+const generateAccessAndRefreshToken = async(userId) => {
+   try{
+      const user = await User.findById(userId)
+      const accessToken = user.generateAccessToken()
+      const refreshToken = user.generateRefreshToken()
+      user.refreshToken = refreshToken
+      user.save({validateBeforeSave:false})
+      return {accessToken,refreshToken}
+   }catch(error){
+      throw new ApiError(500,"Something went wrong while generating access and refresh tokens")
+   }
+}
+
+const loginUser = asyncHandler(async(req,res)=> {
+   //get data from body
+   const {username,email,password} = req.body
+
+   if(!(username || email)){
+      throw new ApiError(400,"Username or email is required")
+   }
+   //username or email based
+   const user = await User.findOne({
+      $or:[{username},{email}]
+   })
+   
+   if(!user){
+      throw new ApiError(404,"User does not exist")
+   }
+   //password check
+   const isPasswordValid = await user.isPasswordCorrect(password)
+   if(!isPasswordValid){
+      throw new ApiError(401,"Invalid user credentials")
+   }
+   //access nad refresh token generation and send
+   const {accessToken,refreshToken} = await generateAccessAndRefreshToken(user._id)
+   const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+   //send cookie
+   const options ={
+      httpOnly:true,
+      secure:true
+   }
+
+   return res.status(200)
+   .cookie("accessToken",accessToken,options)
+   .cookie("refreshToken",refreshToken,options)
+   .json(
+      new apiResponse(200,{
+         user:loggedInUser,accessToken,refreshToken,
+      },
+      "User Logged in successfully")
+   )
+})
+
+export {registerUser,loginUser} 
